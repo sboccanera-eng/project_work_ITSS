7baad0ebfa37a7fd12f25a0c31b5eb70c4c834f5
nohackoonlytaco <denimilo@outlook.com>
2025-10-16 15:31:32 +0200

    prova

---
+++ src/main/java/dev2426/ITSProjectWork/Repository/TirociniRepository.java
@@ -0,0 +1,8 @@
+package dev2426.ITSProjectWork.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TirociniRepository extends JpaRepository<Tirocinio, Long> {
+
+}
---
+++ src/main/java/dev2426/ITSProjectWork/Repository/UtentiRepository.java
@@ -0,0 +1,9 @@
+package dev2426.ITSProjectWork.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UtentiRepository extends JpaRepository<Utente, Long> {
+
+}

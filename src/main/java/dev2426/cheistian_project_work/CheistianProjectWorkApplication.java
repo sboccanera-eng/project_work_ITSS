415e6b0dabdda4d356adbcadea5a30d61ace2a9c
Biziol <88590615+Biziol@users.noreply.github.com>
2025-10-16 12:35:21 +0200

    Main

---
+++ src/main/java/dev2426/cheistian_project_work/CheistianProjectWorkApplication.java
@@ -0,0 +1,13 @@
+package dev2426.cheistian_project_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheistianProjectWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheistianProjectWorkApplication.class, args);
+	}
+
+}

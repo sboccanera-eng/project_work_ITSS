346df1530b7548b3193b14f4c5b38846223edbce
johnanchores <jonathananchores@gmail.com>
2025-10-17 14:52:49 +0200

    hh

--- src/main/java/dev2426/ITSProjectWork/Services/UtentiService.java
+++ src/main/java/dev2426/ITSProjectWork/Services/UtentiService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dev2426.ITSProjectWork.Model.Utente;
 import dev2426.ITSProjectWork.Repository.UtentiRepository;
 
+@Service
 public class UtentiService {
 
 	@Autowired
